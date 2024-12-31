0cd7b5f6c5befd9bbb001d035ca3605da66fc172
LUCIANO131231 <2018110461@udh.edu.pe>
2024-12-31 00:18:36 -0500

    componente reutilizable cardCertificate

--- src/components/Achievements.vue
+++ src/components/Achievements.vue
@@ -1,80 +1,14 @@
 <script setup lang="ts">
-import certificate from "@/assets/images/certificado-001.jpg";
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Certificate from "@/assets/images/certificado-001.jpg";
+import AchievementsItem from "./AchievementsItem.vue";
 </script>
 
 <template>
   <div class="bg-[#fff] min-h-screen flex justify-center items-center">
-    <div class="card mx-10">
-      
-      <div class="flex mb-4 justify-center">
-        <img class="w-full max-w-[600px] sm:w-[450px] h-auto object-contain rounded-md" :src="certificate" alt="Certificate">
-      </div>
-      
-      <span class="block font-bungee text-negro-oxido text-lg text-center sm:text-xl uppercase mb-4 relative overflow-hidden glitch">Diseño Web con HTML5 + CSS</span>
-      <p class="font-suwannaphum text-[#3C3D37] mb-4 max-w-[460px] sm:text-lg  text-justify">Certificación obtenida por completar el curso, un programa de formación digital con una duración de 30 horas.</p>
-      <div class="decor text-black hover:text-white bg-white hover:bg-black uppercase">
-        <FontAwesomeIcon :icon="['fas', 'trophy']" size="2x"/>
-      </div>
-
-    </div>
+    <AchievementsItem 
+      :certificate="Certificate"
+      titulo="Diseño Web con HTML5 + CSS"
+      descripcion="Certificado obtenida por completar el curso, un programa de formación digital con una duración de 30 horas."
+    />
   </div>
 </template>
-
-<style scoped>
-
-.card {
-  width: auto;
-  max-width: 100%;
-  padding: 20px;
-  background: #fff;
-  border: 8px solid #000;
-  box-shadow: 15px 15px 0 #000;
-  position: relative; /* para la etiqueta */
-  transition: transform 0.8s, box-shadow 0.8s;
-}
-
-.card:hover {
-  transform: translate(-5px, -5px);
-  box-shadow: 22px 22px 0 #000;
-}
-
-.decor {
-  position: absolute;
-  bottom: -10px;
-  right: -10px;
-  padding: 5px 10px;
-  transform: rotate(-5deg);
-  transition: transform 0.3s;
-}
-
-.card:hover .decor {
-  transform: rotate(3deg) scale(1);
-}
-
-@keyframes glitch {
-  0% {
-    transform: translate(2px, 2px);
-  }
-
-  25% {
-    transform: translate(-2px, -2px);
-  }
-
-  50% {
-    transform: translate(-2px, 2px);
-  }
-
-  75% {
-    transform: translate(-2px, -2px);
-  }
-
-  100% {
-    transform: translate(2px, 2px);
-  }
-}
-
-.glitch {
-  animation: glitch 0.9s infinite;
-}
-</style>
---
+++ src/components/AchievementsItem.vue
@@ -0,0 +1,95 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const props = defineProps({
+  certificate: {
+    type: String,
+    required: true
+  },
+  titulo: {
+    type: String,
+    required: true
+  },
+  descripcion: {
+    type: String,
+    required: true
+  },
+});
+
+</script>
+
+<template>
+  <div class="card mx-10">
+    <!-- imagenes -->
+    <div class="flex mb-4 justify-center">
+      <img class="w-full max-w-[600px] sm:w-[450px] h-auto object-contain rounded-md" :src="certificate" alt="Certificate">
+    </div>
+    <!-- titulo -->
+    <span class="block font-bungee text-negro-oxido text-lg text-center sm:text-xl uppercase mb-4 relative overflow-hidden glitch">{{titulo}}</span>
+    <!-- descripcion -->
+    <p class="font-suwannaphum text-[#3C3D37] mb-4 max-w-[460px] sm:text-lg  text-justify">{{descripcion}}</p>
+    <!-- icono animacion -->
+    <div class="decor text-black hover:text-white bg-white hover:bg-black uppercase">
+      <FontAwesomeIcon :icon="['fas', 'trophy']" size="2x"/>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+.card {
+  width: auto;
+  max-width: 100%;
+  padding: 20px;
+  background: #fff;
+  border: 8px solid #000;
+  box-shadow: 15px 15px 0 #000;
+  position: relative; /* para la etiqueta */
+  transition: transform 0.8s, box-shadow 0.8s;
+}
+
+.card:hover {
+  transform: translate(-5px, -5px);
+  box-shadow: 22px 22px 0 #000;
+}
+
+.decor {
+  position: absolute;
+  bottom: -10px;
+  right: -10px;
+  padding: 5px 10px;
+  transform: rotate(-5deg);
+  transition: transform 0.3s;
+}
+
+.card:hover .decor {
+  transform: rotate(3deg) scale(1);
+}
+
+@keyframes glitch {
+  0% {
+    transform: translate(2px, 2px);
+  }
+
+  25% {
+    transform: translate(-2px, -2px);
+  }
+
+  50% {
+    transform: translate(-2px, 2px);
+  }
+
+  75% {
+    transform: translate(-2px, -2px);
+  }
+
+  100% {
+    transform: translate(2px, 2px);
+  }
+}
+
+.glitch {
+  animation: glitch 0.9s infinite;
+}
+</style>
