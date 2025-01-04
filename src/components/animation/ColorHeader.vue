b2cbac0545d7857d4fc6ef57f21a3c6d001dfb8d
LUCIANO131231 <2018110461@udh.edu.pe>
2025-01-04 12:27:22 -0500

    cambio de colores

--- src/components/animation/ColorHeader.vue
+++ src/components/animation/ColorHeader.vue
@@ -7,55 +7,59 @@
     <!-- Líneas -->
     <div class="space-y-4">
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-28 bg-gradient-to-l from-[#85A98F] to-[#2A3335]"></div>
-        <div class="h-2 w-16 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-2 rounded-full bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+        <div class="h-2 w-28 bg-[#747264]"></div>
+        <div class="h-2 w-16 bg-[#ECB159]"></div>
+        <div class="h-2 w-2 rounded-full bg-[#B19470]"></div>
+      </div>
+      <div class="flex justify-center space-x-2">
+        <div class="h-2 w-44 bg-[#ECB159]"></div>
       </div>
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-44 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+        <div class="h-2 w-9 bg-[#ADC4CE]"></div>
+        <div class="h-2 w-2 bg-[#ECB159]"></div>
+        <div class="h-2 w-10 bg-[#B19470]"></div>
+        <div class="h-2 w-28 bg-[#ADC4CE]"></div>
       </div>
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-9 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-2 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-10 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-28 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
+        <div class="h-2 w-20 bg-[#F2ECBE]"></div>
+        <div class="h-2 w-10 bg-[#B19470]"></div>
+        <div class="h-2 w-2 rounded-full bg-[#ADC4CE]"></div>
+        <div class="h-2 w-28 bg-[#ECB159]"></div>
       </div>
-      <div class="flex justify-start space-x-2">
-        <div class="h-2 w-20 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-10 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-2 rounded-full bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-28 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-      </div>
-      <div class="flex justify-center space-x-2 h-12">
-        <div class="h-2 w-11 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-2 bg-gradient-to-l from-[#85A98F] to-[#2A3335]"></div>
-        <div class="h-2 w-14 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-24 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+      <div class="flex justify-center space-x-2 h-10">
+        <div class="h-2 w-11 bg-[#ECB159]"></div>
+        <div class="h-2 w-2 bg-[#F2ECBE]"></div>
+        <div class="h-2 w-14 bg-[#B19470]"></div>
+        <div class="h-2 w-24 bg-[#ECB159]"></div>
       </div>
 
-      <div class="h-16 animate-bounce text-4xl text-white font-suwannaphum">My Certificates</div>
+      <div class="h-16 text-4xl text-white font-suwannaphum">My Certificates</div>
 
-      <div class="flex justify-start space-x-2">
-        <div class="h-2 w-20 bg-gradient-to-l from-[#85A98F] to-[#2A3335]"></div>
-        <div class="h-2 w-2 rounded-full bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+      <div class="flex justify-center space-x-2">
+        <div class="h-2 w-32 bg-[#ECB159]"></div>
+        <div class="h-2 w-2 rounded-full bg-[#B19470]"></div>
+        <div class="h-2 w-10 bg-[#F2ECBE]"></div>
       </div>
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-20 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-12 bg-gradient-to-l from-[#85A98F] to-[#2A3335]"></div>
-        <div class="h-2 w-44 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+        <div class="h-2 w-20 bg-[#ADC4CE]"></div>
+        <div class="h-2 w-12 bg-[#B19470]"></div>
+        <div class="h-2 w-32 bg-[#F2ECBE]"></div>
+      </div>
+      <div class="flex justify-center space-x-2">
+        <div class="h-2 w-44 bg-[#F2ECBE]"></div>
+        <div class="h-2 w-9 bg-[#B19470]"></div>
       </div>
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-44 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-9 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
+        <div class="h-2 w-2 rounded-full bg-[#ADC4CE]"></div>
+        <div class="h-2 w-44 bg-[#ADC4CE]"></div>
+        <div class="h-2 w-2 rounded-full bg-[#ADC4CE]"></div>
+        <div class="h-2 w-20 bg-[#ADC4CE]"></div>
       </div>
       <div class="flex justify-start space-x-2">
-        <div class="h-2 w-60 bg-gradient-to-l from-[#85A98F] to-[#2A3335]"></div>
-      </div>
-      <div class="flex justify-start space-x-2">
-        <div class="h-2 w-11 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-2 bg-gradient-to-l from-[#1F4529] to-[#243642]"></div>
-        <div class="h-2 w-16 bg-gradient-to-l from-[#1F4529] to-[#78B3CE]"></div>
-        <div class="h-2 w-28 bg-gradient-l from-[#85A98F] to-[#2A3335]"></div>
+        <div class="h-2 w-11 bg-[#B19470]"></div>
+        <div class="h-2 w-2 bg-[#ECB159]"></div>
+        <div class="h-2 w-16 bg-[#F2ECBE]"></div>
+        <div class="h-2 w-28 bg-[#B19470]"></div>
       </div>
     </div>
 
